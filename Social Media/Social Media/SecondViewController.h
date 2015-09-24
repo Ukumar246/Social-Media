077475bba372249ab42e15d557625edd703f379c
Utkarsh <theutkarsh246@gmail.com>
2015-09-24 17:19:20 -0500

    inital

---
+++ Social Media/Social Media/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Social Media
+//
+//  Created by Utkarsh Kumar on 9/24/15.
+//  Copyright © 2015 Karsh Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ Social Media/Social Media/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Social Media
+//
+//  Created by Utkarsh Kumar on 9/24/15.
+//  Copyright © 2015 Karsh Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
